783b62349a645a20f6d09bf7a5f89ff57c81b0c1
gnoul_ <ttluong4869@gmail.com>
2021-10-06 10:21:10 +0700

    Add functions and logics to controller

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -1,13 +1,45 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const CustomAPIError = require('../errors/custom-error');
+
 const login = async (req, res) => {
-    res.send('Fake Login/Register/Signup Route');
+    const { username, password } = req.body;
+    if (!username || !password) {
+        throw new CustomAPIError('Please provide username and password', 400);
+    }
+
+    // ! For demo purpose only
+    const id = new Date().getDate();
+
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    });
+
+    console.log(token);
+    res.status(200).json({ msg: `User created`, token });
 };
 
 const dashBoard = async (req, res) => {
-    const luckyNumber = Math.floor(Math.random() * 100);
-    res.status(200).json({
-        message: `Hello, User`,
-        secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
-    });
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        throw new CustomAPIError('No token provided', 401);
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const luckyNumber = Math.floor(Math.random() * 100);
+        res.status(200).json({
+            msg: `Hello, ${decoded.username}`,
+            secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+        });
+    } catch (error) {
+        throw new CustomAPIError(
+            'You are not authorized to access this data',
+            401
+        );
+    }
 };
 
 module.exports = { login, dashBoard };
